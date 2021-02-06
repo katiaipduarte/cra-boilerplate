491088466da3fc15cc61f225b499792d848c6c6a
Katia Duarte <katiaipduarte@hotmail.com>
2021-02-06 21:10:00 +0000

    add axios, class example and folder structure

---
+++ src/components/ClassExample.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import { Example } from "../@types/Example";
+
+function ClassExample() {
+  const [posts, setPosts] = useState<Example[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then((response: AxiosResponse<Example[]>) => setPosts(response.data));
+  }, []);
+
+  return (
+    <ul className="posts">
+      {posts.map((post: Example) => (
+        <li className="post" key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ClassExample;
