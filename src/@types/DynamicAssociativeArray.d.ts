40a46bed1d3f103706ec3e9ca5b46c2d9a68d6fa
Katia Duarte <katiaipduarte@users.noreply.github.com>
2021-04-17 18:33:04 +0100

    feat: update CRA boilerplate

--- src/@types/DynamicAssociativeArray.d.ts
+++
@@ -1,3 +0,0 @@
-export interface DynamicAssociativeArray {
-  [key: string]: boolean | string | number;
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./pages/App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './store/store';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+import App from './App';
+import registerIcons from './registerFaIcons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerIcons();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
